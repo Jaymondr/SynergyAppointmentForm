5f0eb7003e99dec2b3e2e5b94e19096bd84c4168
Jaymond Richardson <jaymondrichardson@Jaymonds-MacBook-Pro-2.local>
2024-01-05 16:30:59 -0700

    Adds branch enum

---
+++ SynergyAppointmentForm/Models/Enums/Branch.swift
@@ -0,0 +1,19 @@
+//
+//  Branch.swift
+//  SynergyAppointmentForm
+//
+//  Created by Jaymond Richardson on 1/5/24.
+//
+
+import Foundation
+
+enum Branch: String, CaseIterable {
+    case atlanta = "Atlanta"
+    case austin = "Austin"
+    case dallas = "Dallas"
+    case houston = "Houston"
+    case lasVegas = "Las Vegas"
+    case nashville = "Nashville"
+    case raleigh = "Raleigh"
+    case sanAntonio = "San Antonio"
+}
