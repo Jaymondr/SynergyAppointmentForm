ab6456e950116ec07bcbda906493ea5f0ff03e78
Jaymond Richardson <jaymond@givego.com>
2024-01-09 12:51:06 -0700

    Adds note storyboard wip

---
+++ SynergyAppointmentForm/View Controllers/Notes/NotesViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NotesViewController.swift
+//  SynergyAppointmentForm
+//
+//  Created by Jaymond Richardson on 1/9/24.
+//
+
+import UIKit
+
+class NotesViewController: UIViewController {
+    // MARK: - OUTLETS
+    
+    
+    
+    // MARK: - LIFECYCLE
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupView()
+    }
+    
+    // MARK: - PROPERTIES
+    var form: Form?
+    
+    // MARK: - FUNCTIONS
+    func setupView() {
+        
+    }
+}
