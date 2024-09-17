9013df49e73f283477b699e8effc87fb70812833
Jaymond Richardson <jaymond@givego.com>
2024-09-17 15:23:00 -0400

    Configures image fetch to keep tutorial images in order

--- SynergyAppointmentForm/View Controllers/Tutorial/TutorialViewController.swift
+++ SynergyAppointmentForm/View Controllers/Tutorial/TutorialViewController.swift
@@ -1,10 +1,3 @@
-//
-//  TutorialViewController.swift
-//  SynergyAppointmentForm
-//
-//  Created by Jaymond Richardson on 8/28/24.
-//
-
 import UIKit
 
 class TutorialViewController: UIViewController {
@@ -12,6 +5,9 @@ class TutorialViewController: UIViewController {
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     @IBOutlet weak var stackView: UIStackView!
     @IBOutlet weak var scrollView: UIScrollView!
+    
+    private var imageUrls: [URL] = []
+    private var images: [UIImage?] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,7 +18,7 @@ class TutorialViewController: UIViewController {
     func setupView() {
         activityIndicator.isHidden = false
         activityIndicator.startAnimating()
-
+        
         FirebaseController.shared.getTutorialScreenshotsURL { urls, error in
             if let error = error {
                 print("Failed to get URLs: \(error)")
@@ -42,25 +38,29 @@ class TutorialViewController: UIViewController {
                 return
             }
             
+            self.imageUrls = urls
+            self.images = Array(repeating: nil, count: urls.count)
+            
             let group = DispatchGroup()
-            for url in urls {
+            
+            for (index, url) in urls.enumerated() {
                 group.enter()
                 self.loadImage(from: url) { image in
                     if let image = image {
-                        self.addImageToStackView(image)
+                        self.images[index] = image
                     }
                     group.leave()
                 }
             }
 
             group.notify(queue: .main) {
+                self.updateStackView()
                 self.activityIndicator.stopAnimating()
                 self.activityIndicator.isHidden = true
             }
         }
     }
 
-    
     func loadImage(from url: URL, completion: @escaping (UIImage?) -> Void) {
         let task = URLSession.shared.dataTask(with: url) { data, response, error in
             guard let data = data, error == nil, let image = UIImage(data: data) else {
@@ -72,22 +72,22 @@ class TutorialViewController: UIViewController {
         task.resume()
     }
     
-    func addImageToStackView(_ image: UIImage) {
-        DispatchQueue.main.async {
-            
-            let imageView = UIImageView(image: image)
-             imageView.contentMode = .scaleAspectFit
-             imageView.translatesAutoresizingMaskIntoConstraints = false
-             
-             // Add the image view to the stack view
-            self.stackView.addArrangedSubview(imageView)
-             
-             // Apply constraints
-             NSLayoutConstraint.activate([
-                imageView.widthAnchor.constraint(equalTo: self.scrollView.widthAnchor),
-                imageView.heightAnchor.constraint(equalTo: self.scrollView.heightAnchor)
-             ])
-         }
+    func updateStackView() {
+        self.stackView.arrangedSubviews.forEach { $0.removeFromSuperview() }
+        
+        for image in images {
+            if let image = image {
+                let imageView = UIImageView(image: image)
+                imageView.contentMode = .scaleAspectFit
+                imageView.translatesAutoresizingMaskIntoConstraints = false
+                
+                self.stackView.addArrangedSubview(imageView)
+                
+                NSLayoutConstraint.activate([
+                    imageView.widthAnchor.constraint(equalTo: self.scrollView.widthAnchor),
+                    imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor, multiplier: image.size.height / image.size.width)
+                ])
+            }
+        }
     }
 }
-
